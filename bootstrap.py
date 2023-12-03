945d0cf465a06df208cd7089062fa4091c100f9c
Nathan Mack <nathan@macktribe.com>
2023-12-03 13:01:08 -0600

    Make this template not suck part 2

--- bootstrap.py
+++ bootstrap.py
@@ -1,25 +1,29 @@
 import os
 import subprocess
 
-# Set the path to your Premake executable
-dirname = os.path.dirname(__file__)
-premake_path = os.path.join(dirname, 'premake/premake5.exe')
+def main():
+    # Set the path to your Premake executable
+    dirname = os.path.dirname(__file__)
+    premake_path = os.path.join(dirname, 'premake/premake5.exe')
 
-# Run Premake to generate project files
-subprocess.run([premake_path, 'vs2022'], shell=True, check=True)
+    # Run Premake to generate project files
+    subprocess.run([premake_path, 'vs2022'], shell=True, check=True)
 
-# Build the Google Test library
-subprocess.run(['git', 'submodule', 'update', '--recursive', '--remote'], shell=True, check=True)
+    # Build the Google Test library
+    subprocess.run(['git', 'submodule', 'update', '--recursive', '--remote'], shell=True, check=True)
 
-googletest_path = os.path.join(dirname, 'external/googletest')
+    googletest_path = os.path.join(dirname, 'external/googletest')
 
-os.chdir(googletest_path)
-os.makedirs('build', exist_ok=True)
-os.chdir('build')
-subprocess.run(['cmake', '..'], shell=True, check=True)
-subprocess.run(['cmake', '--build', '.'], shell=True, check=True)
+    os.chdir(googletest_path)
+    os.makedirs('build', exist_ok=True)
+    os.chdir('build')
+    subprocess.run(['cmake', '..'], shell=True, check=True)
+    subprocess.run(['cmake', '--build', '.'], shell=True, check=True)
 
-# Return to the project root
-os.chdir('../../..')
+    # Return to the project root
+    os.chdir('../../..')
 
-print('Bootstrap complete.')
+    print('Bootstrap complete.')
+
+if __name__ == '__main__':
+    main()
--- premake5.lua
+++ premake5.lua
@@ -1,59 +1,41 @@
--- premake5.lua
-workspace "MyCppProject"
-    configurations { "Debug", "Release" }
-
-    -- Set the default platform to x64 for Visual Studio
-    if os.target() == "windows" then
-        platforms { "x64" }
-    end
-
-project "MyCppProject"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++20"
-    targetdir ("bin/%{prj.name}/%{cfg.buildcfg}")
-    objdir ("obj/%{prj.name}/%{cfg.buildcfg}")
-
-    files 
-    { 
-        "src/**.cpp", 
-        "include/**.h" 
-    }
-
-    includedirs 
-    { 
-        "include", 
-        "external/googletest/googletest/include" 
-    }
-
-project "MyCppProjectTests"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++20"
-    targetdir ("bin/%{prj.name}/%{cfg.buildcfg}")
-    objdir ("obj/%{prj.name}/%{cfg.buildcfg}")
+workspace "MyProject"
+    configurations {"Debug", "Release"}
+    platforms { "x64" }
     
-    files 
-    { 
-        "tests/**.cpp" 
-    }
-
-    includedirs 
-    { 
-        "include", 
-        "external/googletest/googletest/include" 
-    }
-
-    links 
-    { 
-        "MyCppProject", 
-        "external/googletest/build/lib/%{cfg.buildcfg}/gtest.lib" 
-    }
+    -- C++ as target language
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    
+    cppdialect "C++20"
 
     filter "configurations:Debug"
+        -- Add the preprocessor definition DEBUG to debug builds
         defines { "DEBUG" }
+        -- Ensure symbols are bundled with debug builds
         symbols "On"
 
     filter "configurations:Release"
+        -- Add the preprocessor definition RELEASE to debug builds
         defines { "RELEASE" }
+        -- Turn on compiler optimizations for release builds
         optimize "On"
+    
+    -- Static library config
+    project "MyProject"
+        kind "StaticLib"
+        -- recursively glob .h and .cpp files in the lib directory
+        files { "include/**.h", "src/**.cpp" }
+
+    project "GoogleTest"
+        kind "StaticLib"
+        files { "external/googletest/googletest/src/gtest-all.cc" }
+        includedirs { "external/googletest/googletest/include", "external/googletest/googletest" }
+    
+    -- Runtime config
+    project "MyProjectTest"
+        kind "ConsoleApp"
+        -- recursively glob .h and .cpp files in the runtime directory
+        files { "tests/**.h", "tests/**.cpp" }
+        -- link the RePlexLib library at runtime
+        includedirs { "include", "external/googletest/googletest/include" }
+        links { "GoogleTest" }
