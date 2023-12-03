f377a834a7807ef46f0eb280c7bc9edaa1901404
Nathan Mack <nathan@macktribe.com>
2023-12-03 12:39:46 -0600

    Make this template not suck part 1

--- bootstrap.py
+++ bootstrap.py
@@ -8,16 +8,10 @@ premake_path = os.path.join(dirname, 'premake/premake5.exe')
 # Run Premake to generate project files
 subprocess.run([premake_path, 'vs2022'], shell=True, check=True)
 
-# Build and clone the Google Test library
+# Build the Google Test library
+subprocess.run(['git', 'submodule', 'update', '--recursive', '--remote'], shell=True, check=True)
 
-# Check if the 'external/googletest' directory exists
 googletest_path = os.path.join(dirname, 'external/googletest')
-if not os.path.exists(googletest_path):
-    # If the directory doesn't exist, clone the submodule
-    subprocess.run(['git', 'submodule', 'add', 'https://github.com/google/googletest.git', 'external/googletest'], shell=True, check=True)
-else:
-    # If the directory exists, update the submodule
-    subprocess.run(['git', 'submodule', 'update', '--recursive', '--remote'], shell=True, check=True)
 
 os.chdir(googletest_path)
 os.makedirs('build', exist_ok=True)
